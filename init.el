b773c283f30f432b14de4a8ed16108af5c02b54c
Geoff T. Wark <wark@posteo.net>
2017-09-19 20:24:51 -0400

    Ignore default REGEXP checks at startup

--- init.el
+++ init.el
@@ -7,24 +7,28 @@
 (message "gc-cons-threshold temporarily set to %S"
 	 gc-cons-threshold)
 
-;; Throw temp files into the system $TEMPDIR
-(setq backup-directory-alist
-      `((".*" . ,temporary-file-directory)))
-(setq auto-save-file-name-transforms
-      `((".*" ,temporary-file-directory t)))
+;; Ignore default REGEXP checks at startup
+;; NOTE: This drastically improves `emacs-init-time'
+(let ((file-name-handler-alist nil)) 
 
-;; TODO: Make this platform independent 
-;; Load some more "private" settings
-(org-babel-load-file "~/../../Nextcloud/org/.private.org")
+  ;; Throw temp files into the system $TEMPDIR
+  (setq backup-directory-alist
+	`((".*" . ,temporary-file-directory)))
+  (setq auto-save-file-name-transforms
+	`((".*" ,temporary-file-directory t)))
 
-;; TODO: Make this platform independent 
-;; Set default directory
-(cd (concat "c:/Users/" user-login-name "/"))
-(setq inhibit-startup-message t)        ; We must disable the startup message
+  ;; TODO: Make this platform independent 
+  ;; Load some more "private" settings
+  (org-babel-load-file "~/../../Nextcloud/org/.private.org")
+
+  ;; TODO: Make this platform independent 
+  ;; Set default directory
+  (cd (concat "c:/Users/" user-login-name "/"))
+  (setq inhibit-startup-message t)      ; We must disable the startup message
 					; for this to work as desired.
-
-;; Revert garbage colletion behaviour back to a more normal, modern value
-(run-with-idle-timer 5 nil (lambda ()
-			     (setq gc-cons-threshold 200000)
-			     (message "gc-cons-threshold restored to %S"
-				      gc-cons-threshold)))
+  
+  ;; Revert garbage colletion behaviour back to a more normal, modern value
+  (run-with-idle-timer 5 nil (lambda ()
+			       (setq gc-cons-threshold 200000)
+			       (message "gc-cons-threshold restored to %S"
+					gc-cons-threshold))))
