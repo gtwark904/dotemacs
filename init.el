52539077dc00686d4de5b01b9cd5fae91134277a
Geoff T. Wark <wark@posteo.net>
2017-09-22 21:57:18 -0400

    Move some things into geoff.org

--- init.el
+++ init.el
@@ -33,22 +33,9 @@
   ;; Load the rest of my configuration
   (org-babel-load-file "~/.emacs.d/geoff.org")
   
-  
-  ;; Throw temp files into the system $TEMPDIR
-  (setq backup-directory-alist
-	`((".*" . ,temporary-file-directory)))
-  (setq auto-save-file-name-transforms
-	`((".*" ,temporary-file-directory t)))
-
   ;; TODO: Make this platform independent 
   ;; Load some more "private" settings
   (org-babel-load-file "~/../../Nextcloud/org/.private.org")
-
-  ;; TODO: Make this platform independent 
-  ;; Set default directory
-  (cd (concat "c:/Users/" user-login-name "/"))
-  (setq inhibit-startup-message t)      ; We must disable the startup message
-					; for this to work as desired.
   
   ;; Revert garbage colletion behaviour back to a more normal, modern value
   (run-with-idle-timer 5 nil (lambda ()
